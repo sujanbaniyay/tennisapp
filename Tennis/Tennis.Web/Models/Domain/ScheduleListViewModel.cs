d376045de06aa71f38595b1d360476cbeb800c58
sujanbaniyay <baniyasujan5678@gmail.com>
2023-09-27 08:01:15 +0300

    Add Login, Schedule List and Schedule models

---
+++ Tennis/Tennis.Web/Models/Domain/ScheduleListViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tennis.Web.Models.Domain
+{
+    public class ScheduleListViewModel
+    {
+        public List<SchedulesMD> Schedules1 { get; set; }
+        public List<Person> Coaches1 { get; set; }
+    }
+}
---
+++ Tennis/Tennis.Web/Models/Domain/SchedulesMD.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tennis.Web.Models.Domain
+{
+    public class SchedulesMD
+    {
+        public int Id { get; set; }
+        // Location
+        [Required]
+        public string? Location { get; set; }
+        // Event name
+        [Required]
+        public string? EventName { get; set; }
+        //Date
+        [Required]
+        public DateTime? Date { get; set; }
+        // Coach Id
+        public int? CoachId { get; set; }
+    }
+}
