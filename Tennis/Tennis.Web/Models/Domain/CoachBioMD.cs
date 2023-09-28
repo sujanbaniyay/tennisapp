76ff92b4ad98590690c75edf5630d55bbfa99924
sujanbaniyay <baniyasujan5678@gmail.com>
2023-09-28 15:09:21 +0300

    Add coach bio model, dbcontext and schedule assignment models

---
+++ Tennis/Tennis.Web/Models/Domain/CoachBioMD.cs
@@ -0,0 +1,10 @@
+﻿namespace Tennis.Web.Models.Domain
+{
+    public class CoachBioMD
+    {
+        // Contain coachId and biography
+        public int Id { get; set; }
+        public int? CoachId { get; set; }
+        public string? Biography { get; set; }
+    }
+}
