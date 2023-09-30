e0230447834fb6a0ff6231281ade183bc8bb6f0e
sujanbaniyay <baniyasujan5678@gmail.com>
2023-09-30 15:19:54 +0300

    Add and Fix person model

---
+++ Tennis/Tennis.Web/Models/Domain/Person.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tennis.Web.Models.Domain
+{
+    public class Person
+    {
+        // id, firstname, lastname, email, age, role
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? EmailAddress { get; set; }
+        // Birth date
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+        // Profile picture
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string? ProfilePic { get; set; }
+        //password
+        [Required]
+        public string? Password { get; set; }
+        // Role is either admin, coach, user
+        public string? Role { get; set; }
+
+    }
+}
